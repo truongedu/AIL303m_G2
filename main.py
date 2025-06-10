dd0eba4bb5c5039855ed8fc31b514d9500804305
LAPTOP-LO1BOSA5\THIEN TRUONG <truongdtce182403@fpt.edu.vn>
2025-06-10 20:30:13 +0700

    thêm biệt danh cho hàm chào của Trường trong file main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from members.hello_truong import print_name
+from members.hello_truong import print_name as truong_greeting
+
 def main():
     print("Hello, Day la loi chao tu cac thanh vien:")
-    print_name()
+    truong_greeting()
 
 
 
