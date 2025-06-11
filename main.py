99fc15038e1828b46cb271af3d7f8d517533c639
LAPTOP-LO1BOSA5\THIEN TRUONG <truongdtce182403@fpt.edu.vn>
2025-06-11 22:29:01 +0700

    hàm huy_greeting, phi_greeting vào main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from members.hello_truong import print_name as truong_greeting
-
+from members.hello_kiet import hello_kiet as kiet_greeting
 def main():
     print("Hello, Day la loi chao tu cac thanh vien:")
     truong_greeting()
-    
+    kiet_greeting()
 
 
 
