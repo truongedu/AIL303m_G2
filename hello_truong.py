e759a6990a32c4702c38f4fae0716df5be85e879
LAPTOP-LO1BOSA5\THIEN TRUONG <truongdtce182403@fpt.edu.vn>
2025-06-12 00:08:58 +0700

    them ham print_greeting cua TRI vao main.py

--- hello_truong.py
+++
@@ -1,5 +0,0 @@
-
-
-
-def print_name():
-    print("Xin chao, minh la Truong - Thanh vien nhom 2!")
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from members.hello_truong import print_name as truong_greeting
+from members.hello_truong import truong_greeting
 from members.hello_kiet import hello_kiet as kiet_greeting
 from members.hello_huy import hello_huy as huy_greeting
 from members.hello_phi import hello_phi as phi_greeting
+from members.hello_tri import print_name as tri_greeting
 
 def main():
     print("Hello, Day la loi chao tu cac thanh vien Group 2:")
@@ -9,6 +10,7 @@ def main():
     kiet_greeting()
     huy_greeting()
     phi_greeting()
+    tri_greeting()
 
 if __name__ == '__main__':
     main()
